0d18a6db6d2d892b0b9e7a32bf9ea28bb3ad5695
N. Taylor Mullen <nimullen@microsoft.com>
2015-07-31 11:09:22 -0700

    React to DNX renames.

--- src/Microsoft.AspNet.Tooling.Razor/AssemblyLoadContextTagHelperTypeResolver.cs
+++ src/Microsoft.AspNet.Tooling.Razor/AssemblyLoadContextTagHelperTypeResolver.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.AspNet.Razor.Runtime.TagHelpers;
-using Microsoft.Framework.Runtime;
+using Microsoft.Dnx.Runtime;
 
 namespace Microsoft.AspNet.Tooling.Razor
 {
